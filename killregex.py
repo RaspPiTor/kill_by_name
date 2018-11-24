0a9be55c1bb0847e1aa7fbee50caf05485bf76dc
Alex Williams <hughaw64@gmail.com>
2018-11-24 12:26:27 +0000

    Added killregex

---
+++ killregex.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import argparse
+import os
+import re
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('regex')
+    parser.add_argument('-s', '--signal', default='15', type=int)
+    parser.add_argument('-c', '--cmdline', action='store_true')
+    args = parser.parse_args()
+    regex = re.compile(args.regex)
+    pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
+    ending = ['comm', 'cmdline'][int(args.cmdline)]
+    for pid in pids:
+        try:
+            with open(os.path.join('/proc', pid, ending)) as file:
+                result = file.read().rstrip('\x00').strip()
+            if regex.fullmatch(result):
+                print('Found match', pid, result)
+                os.kill(int(pid), args.signal)
+        except IOError:
+            pass
+
+if __name__ == '__main__':
+    main()
